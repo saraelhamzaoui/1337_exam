11c4f43e1e2cf31426fe47dc63f675fbccdc0514
Sara El Hamzaoui <elhamzaouisarah10@gmail.com>
2025-04-27 12:12:30 +0100

    ok

--- level00/aff_a/aff_a.c
+++ level00/aff_a/aff_a.c
@@ -7,7 +7,6 @@
     If there are no 'a' characters in the string, the program just writes a newline.
 
     If the number of parameters is not 1, the program displays 'a' followed by a newline.
-
 */
 
 /* ./aff_a "abc" */
@@ -22,25 +21,28 @@
 
 int main (int argc, char *argv[])
 {
-    if (argc == 2)
+    // ./a.out "abc" | cat -e
+
+    // argv[0] : ./a.out
+    // argv[1] : abc
+    // argc : 2
+    if (argc != 2)
     {
-        char *str = argv[1];
+        write(1, "a", 1);
+    }
+    else
+    {
         int i = 0;
-        while (str[i])
+        while (argv[1][i] != '\0')
         {
-            if (str[i] == 'a')
+            if (argv[1][i] == 'a') 
             {
                 write(1, "a", 1);
-                break ;
+                break;
             }
             i++;
         }
     }
-    else // argc == 1, argc == 3,4,5 ...
-    {
-        write(1, "a", 1);
-    }
-
-    write(1, "\n", 1);
+    write (1, "\n", 1);
     return 0;
 }
